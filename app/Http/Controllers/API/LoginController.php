41615f2aea3b80b2032d75aa4306340b77a9465b
Atsiila <qhoeriltila@gmail.com>
2024-07-26 09:00:33 +0700

    Pembaharuan 2

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -1,75 +1,69 @@
 <?php
- 
+
 namespace App\Http\Controllers\Api;
- 
-use App\Http\Controllers\Controller;
+
+use App\Http\Controllers\controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Validator;
 use Hash;
- 
+
 class LoginController extends Controller
 {
-        public function register(Request $request)
-        {
-            $validator = Validator::make($request->all(), [
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|max:255|unique:users',
-                'password' => 'required|string|min:8',
-            ]);
-
-            if ($validator->fails()) {
-            return response()->json($validator->errors());
-            }
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash:: make($request->password),
-            ]);
-            return response()->json([
-                'data' => $user,
-                'success' => true,
-                'message' => 'user berhasil dibuat',
-
-             ]);
-
-        }
-
-    // /**
-    //  * Handle an authentication attempt.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function authenticate(Request $request)
     {
-        if (!auth()->attempt($request->only('email', 'password'))) {
+        if (!auth()->attempt($request->only('email', 'password'))){
             return response()->json([
                 'status' => false,
-                'message' => 'gagal login',
+                'message' => 'Gagal Login',
             ], 401);
         }
-
-        $user = User::where('email', $request->email)->firstOrFail();
+        $user = User::where('email',$request->email)->firstOrfail();
         $token = $user->createToken('auth_token')->plainTextToken;
-        
+
         return response()->json([
             'status' => true,
             'data' => $user,
             'access_token' => $token,
-            'message' => 'login success',
+            'message' => 'Login success',
         ], 200);
-     
-    }
 
-    public function logout() {
+    }
+    public function logout(Request $request) {
         auth()->user()->tokens()->delete();
 
         return response()->json([
             'status' => true,
-            'message' => 'logout success'
+            'message' => 'logout success',
         ], 200);
+    }
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|max:8',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json([
+            'data' => $user,
+            'success' => true,
+            'message' => 'user berhasil di buat',
+        ]);
     }
 }
